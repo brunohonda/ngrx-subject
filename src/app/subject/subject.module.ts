8d6a866d60f700e6d995156145bb4d4bc8a43f2c
Bruno Honda <bruno.honda@live.com>
2022-09-16 02:35:02 -0300

    feat(task-list): replace button to mat-icon-button

--- src/app/subject/subject.module.ts
+++ src/app/subject/subject.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 import { SubjectComponent } from './subject.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TaskListComponent } from './task-list/task-list.component';
-import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -17,6 +19,7 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    MatButtonModule,
     MatIconModule,
   ],
   exports: [
